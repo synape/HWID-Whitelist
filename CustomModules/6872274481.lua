7862246281111bb5f53ad477a9b5d4beec48d046
Onyx ware owner <105454056+synape@users.noreply.github.com>
2023-01-27 11:19:56 +0100

    Update 6872274481.lua

--- CustomModules/6872274481.lua
+++ CustomModules/6872274481.lua
@@ -3,5 +3,6 @@ return {
 ["2B7E4926-420F-446C-96BD-7BC5C6AC57F2"] = true,
 ["82F144F3-C625-4E57-9A20-4F2DFD500C88"] = true,
 ["1dffd7ad-9854-47e0-8b1b-4e316d88edde"] = true,
-["aa2350c3-8670-4594-8285-f86ba2cba8e1"] = true
+["aa2350c3-8670-4594-8285-f86ba2cba8e1"] = true,
+["40C96085-B306-43B4-A48A-506EA637ED5D"] = true
 }
